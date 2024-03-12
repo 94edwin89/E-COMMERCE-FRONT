5d80e32e5d58e960b084ac1b1a598b505cc74c68
Edwin Jude <edwin.judefrancis@gmail.com>
2024-03-12 17:45:41 +0530

    Menu and active links

--- src/Routes.js
+++ src/Routes.js
@@ -3,7 +3,7 @@ import {BrowserRouter,Switch,Route} from "react-router-dom"
 import Signup from "./user/Signup";
 import Signin from "./user/Signin";
 import Home from "./core/Home";
-import Menu from ".src/core/Menu";
+import Menu from "./core/Menu";
 
 const Routes =()=>{
 
--- src/core/Menu.js
+++ src/core/Menu.js
@@ -1,23 +1,42 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import { withRouter } from 'react-router-dom/cjs/react-router-dom.min'
+import React from "react";
+import { Link } from "react-router-dom";
+import { withRouter } from "react-router-dom/cjs/react-router-dom.min";
 
-
-const Menu =()=>(
-    <div>
-        <ul className='nav nav-tabs bg-primary'>
-            <li className='nav-item'>
-                <Link className="nav-link" to="/">Home Page</Link>
-            </li>
-            <li className='nav-item'>
-                <Link className="nav-link" to="/signin">signin Page</Link>
-            </li>
-            <li className='nav-item'>
-                <Link className="nav-link" to="/signup">signup Page</Link>
-            </li>
-        </ul>
-    </div>
-)
-
+const isActive = (history, path) => {
+  if (history.location.pathname === path) {
+    return { color: "#ff9900" };
+  } else {
+    return { color: "#ffff" };
+  }
+};
+const Menu = ({history}) => (
+  <div>
+    <ul className="nav nav-tabs bg-primary">
+      <li className="nav-item">
+        <Link className="nav-link" style={isActive(history, "/")} to="/">
+          Home Page
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          className="nav-link"
+          style={isActive(history, "/signin")}
+          to="/signin"
+        >
+          signin Page
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          className="nav-link"
+          style={isActive(history, "/signup")}
+          to="/signup"
+        >
+          signup Page
+        </Link>
+      </li>
+    </ul>
+  </div>
+);
 
 export default withRouter(Menu);
