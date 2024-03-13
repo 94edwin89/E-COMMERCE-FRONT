e5e49c7e47893bf7128d36e47b8016782f524093
Edwin Jude <edwin.judefrancis@gmail.com>
2024-03-13 14:20:46 +0530

    user signup success and error

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 import Layout from "../core/Layout";
 import { API } from "../config";
 
@@ -11,35 +12,50 @@ const Signup = () => {
     success: false,
   });
 
-
-  const {name,email,password}=values
+  const { name, email, password, success, error } = values;
 
   const handleChange = (name) => (event) => {
     setValues({ ...values, error: false, [name]: event.target.value });
   };
 
-  const signup=(user)=>{
+  const signup = (user) => {
     //console.log(name,email,password);
-    fetch(`${API}/signup`,{
-        method:"POST",
-        headers:{
-            Accept:'application/json',
-            "Content-Type":"application/json"
-        },
-        body:JSON.stringify(user)
-    })
-    .then(Response =>{
-        return Response.json();
-    })
-    .catch(err =>{
-        console.log(err)
-    })
-  }
 
-  const clickSubmit = (event)=>{
-    event.preventDefault()
-    signup({name,email,password})
-  }
+    return fetch(`${API}/signup`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((Response) => {
+        return Response.json();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const clickSubmit = (event) => {
+    event.preventDefault();
+    setValues({ ...values, error: false });
+
+    signup({ name, email, password }).then((data) => {
+      if (data.error) {
+        setValues({ ...values, error: data.error, success: false });
+      } else {
+        setValues({
+          ...values,
+          name: "",
+          email: "",
+          password: "",
+          error: "",
+          success: true,
+        });
+      }
+    });
+  };
   const signUpForm = () => (
     <form>
       <div className="form-group">
@@ -48,6 +64,7 @@ const Signup = () => {
           onChange={handleChange("name")}
           type="text"
           className="form-control"
+          value={name}
         />
       </div>
 
@@ -57,6 +74,7 @@ const Signup = () => {
           onChange={handleChange("email")}
           type="email"
           className="form-control"
+          value={email}
         />
       </div>
 
@@ -66,20 +84,44 @@ const Signup = () => {
           onChange={handleChange("password")}
           type="password"
           className="form-control"
+          value={password}
         />
       </div>
 
-      <button onClick={clickSubmit} class="btn btn-primary">Submit</button>
+      <button onClick={clickSubmit} class="btn btn-primary">
+        Submit
+      </button>
     </form>
   );
+
+  const showError = () => (
+    <div
+      className="alert alert-danger"
+      style={{ display: error ? "" : "none" }}
+    >
+      {error}
+    </div>
+  );
+
+  const showSuccess = () => (
+    <div
+      class="font-monospace"
+      className="'alert alert-info"
+      style={{ display: success ? "" : "none" }}
+    >
+      New account is created. Please <Link to="/signin">Signin</Link>
+    </div>
+  );
+
   return (
     <Layout
       title="Signup"
       description="Signup to Node React E-commerce App"
       className="container col-md-8 offset-md-2"
     >
+      {showError()}
+      {showSuccess()}
       {signUpForm()}
-      
     </Layout>
   );
 };
