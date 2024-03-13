d829f9178569a16e81ec4c384f5715b2acbd28ba
Edwin Jude <edwin.judefrancis@gmail.com>
2024-03-13 23:26:56 +0530

    code refactoring-Signup

---
+++ src/auth/index.js
@@ -0,0 +1,21 @@
+import { API } from "../config";
+
+
+export const signup = (user) => {
+    //console.log(name,email,password);
+
+    return fetch(`${API}/signup`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((Response) => {
+        return Response.json();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
