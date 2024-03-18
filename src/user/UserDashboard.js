067f968531237ed0b048f39efd5dd5112ae2c706
Edwin Jude <edwin.judefrancis@gmail.com>
2024-03-18 18:43:20 +0530

    Links on user dashboard

--- src/user/UserDashboard.js
+++ src/user/UserDashboard.js
@@ -1,17 +1,38 @@
 import React from "react";
 import Layout from "../core/Layout";
 import { isAuthenticated } from "../auth";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+
+
 
 const Dashboard = () => {
   const {
-    user: { _id, name, email, role },
+    user: {_id,  name, email, role },
   } = isAuthenticated();
-  return (
-    <Layout
-      title="Dashboard"
-      description="User Dashboard"
-      className="container"
-    >
+
+  const userLInks = () => {
+
+    return (
+      <div className="card">
+        <h4 className="card-header">User Links</h4>
+        <ul className="list-group">
+          <li className="list-group-item">
+            <Link className="nav-link" to="/cart">
+              My Cart
+            </Link>
+          </li>
+          <li className="list-group-item">
+            <Link className="nav-link" to="/profile/update">
+              Update Profile
+            </Link>
+          </li>
+        </ul>
+      </div>
+    );
+  };
+
+  const userInfo = () => {
+    return (
       <div className="card mb-5">
         <h3 className="card-header">User Information</h3>
         <ul className="list-group">
@@ -22,11 +43,34 @@ const Dashboard = () => {
           </li>
         </ul>
       </div>
+    );
+  };
+
+
+  const purchaseHistory = () => {
+    return (
       <div className="card md-5">
         <h2 className="card-header">Purchase History</h2>
         <ul className="list-group">
           <li className="list-group-item">history</li>
         </ul>
+      </div>
+    );
+  };
+
+
+  return (
+    <Layout
+      title="Dashboard"
+      description={`G'day ${name}!!`}
+      className="container-fluid"
+    >
+      <div className="row">
+        <div className="col-3">{userLInks()}</div>
+        <div className="col-9">
+          {userInfo()}
+          {purchaseHistory()}
+        </div>
       </div>
     </Layout>
   );
