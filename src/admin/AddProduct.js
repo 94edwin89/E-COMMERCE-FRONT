4fe7b7576f906276963a191c50e248e4f318dfeb
Edwin Jude <edwin.judefrancis@gmail.com>
2024-03-21 10:34:42 +0530

    creating product with categories

--- src/admin/AddProduct.js
+++ src/admin/AddProduct.js
@@ -2,7 +2,7 @@ import { React, useState, useEffect } from "react";
 import Layout from "../core/Layout";
 import { isAuthenticated } from "../auth";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
-import { createProduct } from "./apiAdmin";
+import { createProduct, getCategories } from "./apiAdmin";
 
 const AddProduct = () => {
   const { user, token } = isAuthenticated();
@@ -39,11 +39,23 @@ const AddProduct = () => {
     formData,
   } = values;
 
+  // load categories and set form data
+
+  const init = () => {
+    getCategories().then((data) => {
+      if (data.error) {
+        setValues({ ...values, error: data.error });
+      } else {
+        setValues({ ...values, categories: data, formData: new FormData() });
+      }
+    });
+  };
+
   useEffect(() => {
-    setValues({ ...values, formData: new FormData() });
+    init();
   }, []);
 
-  const handleChange = name => event => {
+  const handleChange = (name) => (event) => {
     const value = name === "photo" ? event.target.files[0] : event.target.value;
     formData.set(name, value);
     setValues({ ...values, [name]: value });
@@ -119,14 +131,25 @@ const AddProduct = () => {
       <div className="form-group">
         <label className="text-muted">Category</label>
         <select onChange={handleChange("category")} className="form-control">
-          <option value="65fab1d680d3d1e74d77d9e6">Python</option>
-          <option value="65fab1d680d3d1e74d77d9e6">Python</option>
+          <option>Please select</option>
+          {categories &&
+            categories.map(
+              (
+                c,
+                i // c is for categories and i is for index
+              ) => (
+                <option key={i} value={c._id}>
+                  {c.name}
+                </option>
+              )
+            )}
         </select>
       </div>
 
       <div className="form-group">
         <label className="text-muted">Shipping</label>
         <select onChange={handleChange("shipping")} className="form-control">
+          <option>Please select</option>
           <option value="0">No</option>
           <option value="1">Yes</option>
         </select>
@@ -144,15 +167,44 @@ const AddProduct = () => {
 
       <button className="btn btn-outline-primary">Create Product</button>
     </form>
-  
   );
+
+  const showError = () => (
+    <div
+      className="alert alert-danger"
+      style={{ display: error ? "" : "none" }}
+    >
+      {error}
+    </div>
+  );
+  const showSuccess = () => (
+    <div
+      className="alert alert-info"
+      style={{ display: createProduct ? "" : "none" }}
+    >
+      <h2>{`${createdProduct}`} is created!</h2>
+    </div>
+  );
+
+  const showLoading = () => {
+    loading && (
+      <div className="alert alert-success">
+        <h2>Loading. . .</h2>
+      </div>
+    );
+  };
   return (
     <Layout
       title="Add a new product"
       description={`G'day ${user.name}, ready to add a new product?`}
     >
       <div className="row">
-        <div className="col-md-8 offset-md-2">{newPostForm()}</div>
+        <div className="col-md-8 offset-md-2">
+          {showLoading()}
+           {showSuccess()} 
+           {showError()}
+          {newPostForm()}
+        </div>
       </div>
     </Layout>
   );
