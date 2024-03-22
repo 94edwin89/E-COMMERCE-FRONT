bea455e71c547a255671d381bb91f0af46181b19
Edwin Jude <edwin.judefrancis@gmail.com>
2024-03-22 18:34:09 +0530

    handle categories,passing filter&filter category

--- src/core/Shop.js
+++ src/core/Shop.js
@@ -2,8 +2,14 @@ import React, { useState, useEffect } from "react";
 import Layout from "./Layout";
 import Card from "./Card";
 import { getCategories } from "./apiCore";
+import Checkbox from "./Checkbox";
+import RadioBox from "./RadioBox";
+import { prices } from "./fixedPirces";
 
 const Shop = () => {
+  const [myFilters, setMyFilters] = useState({
+    filters: { category: [], price: [] },
+  });
   const [categories, setCategories] = useState([]);
   const [error, setError] = useState([]);
 
@@ -20,6 +26,14 @@ const Shop = () => {
   useEffect(() => {
     init();
   }, []);
+
+  const handleFilters = (filters, filterBy) => {
+    //console.log("SHOP", filters, filterBy);
+    const newFilters = { ...myFilters };
+    newFilters.filters[filterBy] = filters;
+    setMyFilters(newFilters);
+  };
+
   return (
     <Layout
       title="Shop Page"
@@ -27,8 +41,25 @@ const Shop = () => {
       className="container-fluid"
     >
       <div className="row">
-        <div className="col-4">{JSON.stringify(categories)}</div>
-        <div className="col-8">right</div>
+        <div className="col-4">
+          <h4>Filter By categories</h4>
+          <ul>
+            <Checkbox
+              categories={categories}
+              handleFilters={(filters) => handleFilters(filters, "category")}
+            />
+          </ul>
+
+          <h4>Filter By Price range</h4>
+          <div>
+            <RadioBox
+              prices={prices}
+              handleFilters={(filters) => handleFilters(filters, "price")}
+            />
+          </div>
+          
+        </div>
+        <div className="col-8">{JSON.stringify(myFilters)}</div>
       </div>
     </Layout>
   );
