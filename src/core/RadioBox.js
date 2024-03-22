88c4b8dddf99f0db70ada45a207de7b01b8dc103
Edwin Jude <edwin.judefrancis@gmail.com>
2024-03-22 19:43:58 +0530

    fitler with price range &show on shop page

--- src/core/RadioBox.js
+++ src/core/RadioBox.js
@@ -1,16 +1,19 @@
 import React, {useState, useEffect, Fragment} from "react";
 
-const RadioBox = ({prices}) =>{
+
+const RadioBox = ({prices, handleFilters}) =>{
   const [value,setValue] = useState(0)
 
-  const handleChange =()=>{
-
+  const handleChange =(event)=>{
+        handleFilters(event.target.value)
+        setValue(event.target.value)
   }
 
   return prices.map((p, i) => (
     <div key={i}>
       <input
         onChange={handleChange}
+        name={p}
         value={`${p._id}`}
         type="radio"
         className="mr-2 ml-4"
--- src/core/Shop.js
+++ src/core/Shop.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Layout from "./Layout";
 import Card from "./Card";
-import { getCategories } from "./apiCore";
+import { getCategories, getFilteredProducts } from "./apiCore";
 import Checkbox from "./Checkbox";
 import RadioBox from "./RadioBox";
 import { prices } from "./fixedPirces";
@@ -11,7 +11,10 @@ const Shop = () => {
     filters: { category: [], price: [] },
   });
   const [categories, setCategories] = useState([]);
-  const [error, setError] = useState([]);
+  const [error, setError] = useState(false);
+  const [limit, setLimit] = useState(6);
+  const [skip, setSkip] = useState(0);
+  const [FilteredResults, setFilteredResults] = useState(0);
 
   const init = () => {
     getCategories().then((data) => {
@@ -23,15 +26,47 @@ const Shop = () => {
     });
   };
 
+  const loadFilteredResults =( newFilters) => {
+    //console.log(newFilters)
+    getFilteredProducts(skip, limit, newFilters).then(data => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setFilteredResults(data);
+      }
+    });
+  };
+
   useEffect(() => {
     init();
+    loadFilteredResults(skip, limit, myFilters.filters);
   }, []);
 
   const handleFilters = (filters, filterBy) => {
     //console.log("SHOP", filters, filterBy);
     const newFilters = { ...myFilters };
     newFilters.filters[filterBy] = filters;
+
+    if (filterBy === "price") {
+      let priceValues = handlePrice(filters);
+      newFilters.filters[filterBy] = priceValues;
+    }
+
+    loadFilteredResults(myFilters.filters);
     setMyFilters(newFilters);
+  };
+
+  const handlePrice = value => {
+    const data = prices;
+    let array = [];
+
+    for (let key in data) {
+      if (data[key]._id === parseInt(value)) {
+        array = data[key].array;
+      }
+    }
+
+    return array;
   };
 
   return (
@@ -57,9 +92,8 @@ const Shop = () => {
               handleFilters={(filters) => handleFilters(filters, "price")}
             />
           </div>
-          
         </div>
-        <div className="col-8">{JSON.stringify(myFilters)}</div>
+        <div className="col-8">{JSON.stringify(FilteredResults)}</div>
       </div>
     </Layout>
   );
