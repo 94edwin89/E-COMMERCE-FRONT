7c536b4bad6f3a3d50a0b12a460db7cbf49c9e83
Edwin Jude <edwin.judefrancis@gmail.com>
2024-03-23 15:50:31 +0530

    Implementing search

--- src/core/Search.js
+++ src/core/Search.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
-import Layout from "./Layout";
-import { getCategories } from "./apiCore";
+import { getCategories, list } from "./apiCore";
 import Card from "./Card";
 
 const Search = () => {
@@ -29,26 +28,71 @@ const Search = () => {
     loadCategories();
   }, []);
 
-  const searchSubmit = () => {};
+  const searchData = () => {
+    // console.log(search,category)
+    if (search) {
+      list({ search: search || undefined, category: category }).then(
+        (response) => {
+          if (response.error) {
+            console.log(response.error);
+          } else {
+            setData({ ...data, results: response, search: true });
+          }
+        }
+      );
+    }
+  };
+  const searchSubmit = (e) => {
+    e.preventDefault();
+    searchData();
+  };
 
-  const handleChange = () => {};
+  const handleChange = name => event => {
+    setData({ ...data, [name]: event.target.value, searched: event.target.value !==''});
+  };
+
+
+  const searchMessage = (searched,results) =>{
+
+    if (searched && results.length > 0){
+      return `Found ${results.length} products`;
+    }
+    if (searched && results.length < 1){
+      return `No products found`;
+      
+    }
+    
+  }
+
+  const searchedProducts = (results = []) => {
+    return (
+      <div>
+        <h2 className="mt-4 mb-4">
+          {searchMessage(searched,results)}
+        </h2>
+        <div className="row">
+        {results.map((product, i) => (
+          <Card key={i} product={product} />
+        ))}
+      </div>
+      </div>
+    );
+  };
 
   const searchForm = () => (
     <form onSubmit={searchSubmit}>
       <span className="input-group-text">
         <div className="input-group input-group-lg">
-            <div className="input-group-prepend">
-                <select className="btn mr-2" 
-                        onChange={handleChange('category')}>
-                            
-                    <option value="All" >Pick Category</option>
-                    {categories.map((c,i) =>(
-                        <option key={i} value={c._id}>
-                             {c.name}
-                         </option>
-                    ))}
-                </select>
-            </div>
+          <div className="input-group-prepend">
+            <select className="btn mr-2" onChange={handleChange("category")}>
+              <option value="All">All</option>
+              {categories.map((c, i) => (
+                <option key={i} value={c._id}>
+                  {c.name}
+                </option>
+              ))}
+            </select>
+          </div>
           <input
             type="search"
             className="form-control"
@@ -56,18 +100,17 @@ const Search = () => {
             placeholder="Search by name"
           />
         </div>
-        <div className="btn input-group-append" 
-             style={{border:'none'}}>
-
-            <button className="input-group-text">Search</button>
+        <div className="btn input-group-append" style={{ border: "none" }}>
+          <button className="input-group-text">Search</button>
         </div>
       </span>
     </form>
-  )
+  );
 
   return (
     <div className="row ">
       <div className="container mt-3 mb-3">{searchForm()}</div>
+      <div className="container-fluid mb-3">{searchedProducts(results)}</div>
     </div>
   );
 };
