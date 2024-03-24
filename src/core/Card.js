fbecbd52a93d7ac87f4897da0e3031c97b4c6325
Edwin Jude <edwin.judefrancis@gmail.com>
2024-03-24 23:35:16 +0530

    single page product details & related products

--- src/core/Card.js
+++ src/core/Card.js
@@ -1,46 +1,69 @@
 import React from "react";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
 import ShowImage from "./ShowImage";
+import moment from "moment"
 
-
-
-const Card = ({ product,showViewProductButton = true }) => {
-
-  const showViewButton = (showViewProductButton) =>{
+const Card = ({ product, showViewProductButton = true }) => {
+  const showViewButton = (showViewProductButton) => {
     return (
       showViewProductButton && (
-        <Link to={`/product/${product._id}`} >
-        <button className="btn btn-outline-primary mt-2 mb-2" style={{ marginRight: '10px' }} >
-          View Product
-        </button>
-          </Link>
+        <Link to={`/product/${product._id}`}>
+          <button
+            className="btn btn-outline-primary mt-2 mb-2"
+            style={{ marginRight: "10px" }}
+          >
+            View Product
+          </button>
+        </Link>
       )
+    );
+  };
+
+  const showAddToCartButton =() =>{
+    return(
+      <button className="btn btn-outline-warning mt-2 mb-2 ">
+    Add to cart
+  </button>
     )
   }
+
+  const showStock = (quantity) =>{
+    return quantity > 0 ? <span className="badge text-bg-primary ">In Stock</span> : 
+        <span className="badge text-bg-primary">Out of Stock</span>
+  }
+
+
+
   return (
-    
-      <div className="card">
-        <div className="card-header">{product.name}</div>
-        <div className="card-body">
+    <div className="card">
+      <div className="card-header name">{product.name}</div>
+      <div className="card-body">
+        <ShowImage item={product} url="product" />
 
-            <ShowImage item={product} url="product"/>
+        <p className="lead mt-2">
+          {product.description.substring(0, 100)}
+          </p>
 
-            
-          <p>{product.description.substring(0,100)}</p>
-          <p>${product.price}</p>
-          
-            {showViewButton(showViewProductButton)}
-            {/* <button className="btn btn-outline-primary mt-2 mb-2 mr-2"
-                    style={{ marginRight: '10px' }}  >
-              View Product
-            </button> */}
+        <p className="black-10">${product.price}</p>
+
+        <p className="black-9">Category:{product.category && 
+                               product.category.name}
+        </p>
+
+        <p className="black-8">
+          Added on {moment(product.createdAt).fromNow()}
+        </p>
+
         
-          <button className="btn btn-outline-warning mt-2 mb-2 ">
-            Add to cart
-          </button>
-        </div>
+       {showStock(product.quantity)}
+       <br />
+        {showViewButton(showViewProductButton)}
+
+        {showAddToCartButton()}
+       
+
       </div>
-   
+    </div>
   );
 };
 
