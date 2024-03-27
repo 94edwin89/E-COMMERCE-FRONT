d65fb1f79aad732c99af2f62496c4d434aec7268
Edwin Jude <edwin.judefrancis@gmail.com>
2024-03-27 19:35:13 +0530

    ignore small changes in cart

--- src/core/Cart.js
+++ src/core/Cart.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import Layout from "./Layout";
 import { getCart } from "./cartHelpers";
 import Card from "./Card";
-// import Checkout from "./Checkout";
+import Checkout from "./Checkout";
 
 const Cart = () => {
     const [items, setItems] = useState([]);
@@ -50,7 +50,7 @@ const Cart = () => {
                 <div className="col-6">
                     <h2 className="mb-4">Your cart summary</h2>
                     <hr />
-                    {/* <Checkout products={items} /> */}
+                    <Checkout products={items} />
                 </div>
             </div>
         </Layout>
